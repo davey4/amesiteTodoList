b8afecced20e40f0fdd8a94d5eb99de4dc3d74c8
David Martel <daveymartel@gmail.com>
2021-04-16 15:01:55 -0400

    added more tags

--- client/src/app/todos/todos.component.ts
+++ client/src/app/todos/todos.component.ts
@@ -15,6 +15,10 @@ export class TodosComponent implements OnInit {
   high: boolean = false;
   medium: boolean = false;
   low: boolean = false;
+  work: boolean = false;
+  exercise: boolean = false;
+  leisure: boolean = false;
+  errand: boolean = false;
 
   constructor(private stateService: StateService) {}
 
@@ -36,6 +40,10 @@ export class TodosComponent implements OnInit {
     this.high = false;
     this.medium = false;
     this.low = false;
+    this.work = false;
+    this.exercise = false;
+    this.leisure = false;
+    this.errand = false;
     this.tags = [];
     this.formError = false;
     form.reset();
@@ -86,6 +94,54 @@ export class TodosComponent implements OnInit {
     } else {
       const newTags = this.tags.filter((el) => el !== 'Low Priority');
       this.low = !this.low;
+      this.tags = newTags;
+    }
+  }
+
+  addWork($event) {
+    $event.preventDefault();
+    if (!this.work) {
+      this.tags.push('Work');
+      this.work = !this.work;
+    } else {
+      const newTags = this.tags.filter((el) => el !== 'Work');
+      this.work = !this.work;
+      this.tags = newTags;
+    }
+  }
+
+  addExercise($event) {
+    $event.preventDefault();
+    if (!this.exercise) {
+      this.tags.push('Exercise');
+      this.exercise = !this.exercise;
+    } else {
+      const newTags = this.tags.filter((el) => el !== 'Exercise');
+      this.exercise = !this.exercise;
+      this.tags = newTags;
+    }
+  }
+
+  addLeisure($event) {
+    $event.preventDefault();
+    if (!this.leisure) {
+      this.tags.push('Leisure');
+      this.leisure = !this.leisure;
+    } else {
+      const newTags = this.tags.filter((el) => el !== 'Leisure');
+      this.leisure = !this.leisure;
+      this.tags = newTags;
+    }
+  }
+
+  addErrand($event) {
+    $event.preventDefault();
+    if (!this.errand) {
+      this.tags.push('Errand');
+      this.errand = !this.errand;
+    } else {
+      const newTags = this.tags.filter((el) => el !== 'Errand');
+      this.errand = !this.errand;
       this.tags = newTags;
     }
   }
